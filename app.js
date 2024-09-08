7ed71814c23d473f12c9fec7a2f425428f1fb912
yashNaiya <yashnaiya16@gmail.com>
2024-09-08 16:41:47 +0530

    commit

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ app.use("/api/users", require("./routes/userRoutes") )
 app.use("/api/books", require("./routes/bookRoutes") )
 app.use("/api/transactions", require("./routes/transactionRoutes") )
 
+const origin = ["https://yash-library-management.netlify.app", "http://localhost:5000"]
+app.use(cors({
+    credentials: true,
+    origin: origin,
+    methods: ["GET", "POST","PUT"],
+    preflightContinue: true,
+}));
 
-
-app.listen(port,()=>{
+app.listen(port, '0.0.0.0',()=>{
   console.log(`app is running at port ${port}`)
 })

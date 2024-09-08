6e5a9070c1e8038c203161b095de18aa89f32e16
yashNaiya <yashnaiya16@gmail.com>
2024-09-08 17:02:33 +0530

    commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config()
+const cors = require(`cors`)
 const app = express();
 require(`./config/dbConnect`)
 const port = process.env.PORT
@@ -8,7 +9,7 @@ app.use("/api/users", require("./routes/userRoutes") )
 app.use("/api/books", require("./routes/bookRoutes") )
 app.use("/api/transactions", require("./routes/transactionRoutes") )
 
-const origin = ["https://yash-library-management.netlify.app", "http://localhost:5000"]
+const origin = ["https://yash-library-management-system.onrender.com", "http://localhost:5000"]
 app.use(cors({
     credentials: true,
     origin: origin,
